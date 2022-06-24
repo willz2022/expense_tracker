b1a09ede3fdbe27cc4bdff8fa4ae2e1f0ddadd4d
willz2022 <102101355+willz2022@users.noreply.github.com>
2022-06-24 12:06:13 -0400

    mongo config

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,13 @@
 import express from "express";
 import { db } from "./db.js";
+import mongoose from "mongoose";
+import dotenv from 'dotenv'
+
+dotenv.config()
+mongoose.connect(process.env.MONGODB_URI).then(()=>{
+    console.log('connected to db')
+}).catch(err=>{console.log(err.message)})
+
 
 const app = express()
 
@@ -7,7 +15,7 @@ app.get('/api/transactions',(req, res)=>{
     res.send(db.transactions)
 })
 
-const port = process.env.PORT || 3500
+const port = process.env.PORT || 5000
 
 app.listen(port, ()=>{
     console.log(`serve at http://localhost:${port}`)
--- src/features/api/apiSlice.js
+++ src/features/api/apiSlice.js
@@ -3,7 +3,8 @@ import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
 
 export const transactionsSlice = createApi({
     reducerPath: 'api',
-    baseQuery: fetchBaseQuery ({ baseUrl:'http://localhost:3500/'}),
+    //baseQuery: fetchBaseQuery ({ baseUrl:'http://localhost:3500/'}),
+    baseQuery: fetchBaseQuery ({ baseUrl:'http://localhost:5000/api/'}),
     tagTypes: ['Transactions'],
     endpoints: (builder)=>({})
 })
